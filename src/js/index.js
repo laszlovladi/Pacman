cfc9c55805c8ba6de3c0b6ca959f7b9c00535c08
laszlovladi <laszlo.vladi@gmail.com>
2020-02-07 11:01:13 +0100

    created pacman class, added moveRight() function

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,49 @@
+class Pacman {
+  constructor (xpos, mouth) {
+    this.xpos = xpos;
+    this.mouth = mouth;
+  }
+
+  moveRight() {
+    this.xpos += 85;
+    this.update();
+  }
+
+  render() {
+    this.element = document.createElement('div');
+    this.element.className = 'entity entity--pac pacboy-active-light';
+    
+    document.addEventListener('keydown', event => {
+      if(event.key === 'ArrowRight') {
+        if (this.element.style.backgroundPositionX === '85px') {
+          this.element.style.backgroundPositionX = '0';
+          }
+        else {
+          this.element.style.backgroundPositionX = '85px';
+        }
+        this.moveRight();
+      }
+    });
+
+    this.update();
+
+    return this.element;
+  }
+
+  update() {
+    // this.element = document.querySelector('.entity--pac');
+    this.element.style.left = `${this.xpos}px`;
+  }
+
+  mount() {
+    const contElem = document.querySelector('.container');
+    this.render();
+    contElem.appendChild(this.element);
+  }
+  
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  const pacElement = document.querySelector('.entity--pac');
-  document.addEventListener('keydown', event => {
-    if(event.key === 'ArrowRight') {
-      if (pacElement.style.backgroundPositionX === '85px') {
-        pacElement.style.backgroundPositionX = '0';
-      }
-      else {
-        pacElement.style.backgroundPositionX = '85px';
-      }
-    }
-  });
+  const pacman = new Pacman(0, 0);
+  pacman.mount();
 });
