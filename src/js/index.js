ec0d1d957c79b19c9b531cb80ac1d4a71e7388ed
ondrejtrisc <57498031+ondrejtrisc@users.noreply.github.com>
2020-02-07 11:26:36 +0100

    packman is moving

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,39 @@
 class Pacman {
-  constructor (xpos, mouth) {
+  constructor (xpos, ypos, mouth) {
     this.xpos = xpos;
+    this.ypos = ypos;
     this.mouth = mouth;
   }
 
-  moveRight() {
-    this.xpos += 85;
+  move(key) {    
+    if (key === 'ArrowRight') {
+      this.xpos += 85;
+      this.element.style.backgroundPositionY = '0';
+    }
+    else if (key === 'ArrowLeft') {
+      this.xpos -= 85;
+      this.element.style.backgroundPositionY = '255px';
+    }
+    else if (key === 'ArrowUp') {
+      this.ypos -= 85;
+      this.element.style.backgroundPositionY = '85px';
+    }
+    else if (key === 'ArrowDown') {
+      this.ypos += 85;
+      this.element.style.backgroundPositionY = '170px';
+    }
     this.update();
+  }
+
+  changeMouth() {
+    if (this.mouth === '85px') {
+      this.element.style.backgroundPositionX = '0';
+      this.mouth = '0';
+      }
+    else {
+      this.element.style.backgroundPositionX = '85px';
+      this.mouth = '85px';
+    }
   }
 
   render() {
@@ -14,14 +41,10 @@ class Pacman {
     this.element.className = 'entity entity--pac pacboy-active-light';
     
     document.addEventListener('keydown', event => {
-      if(event.key === 'ArrowRight') {
-        if (this.element.style.backgroundPositionX === '85px') {
-          this.element.style.backgroundPositionX = '0';
-          }
-        else {
-          this.element.style.backgroundPositionX = '85px';
-        }
-        this.moveRight();
+      if(event.key === 'ArrowRight' || event.key === 'ArrowLeft' 
+      || event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+        this.changeMouth();
+        this.move(event.key);
       }
     });
 
@@ -33,6 +56,7 @@ class Pacman {
   update() {
     // this.element = document.querySelector('.entity--pac');
     this.element.style.left = `${this.xpos}px`;
+    this.element.style.top = `${this.ypos}px`;
   }
 
   mount() {
@@ -44,6 +68,6 @@ class Pacman {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const pacman = new Pacman(0, 0);
+  const pacman = new Pacman(0, 0, 0);
   pacman.mount();
 });
