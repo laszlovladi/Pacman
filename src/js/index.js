0c4332435e0db231ffa4de115d0d7f46be09ff5c
laszlovladi <laszlo.vladi@gmail.com>
2020-02-07 12:03:17 +0100

    pacman respects the borders

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,36 @@
 class Pacman {
-  constructor (xpos, ypos, mouth) {
+  constructor (xpos, ypos, mouth, maxX, maxY) {
     this.xpos = xpos;
     this.ypos = ypos;
     this.mouth = mouth;
+    this.maxX = maxX;
+    this.maxY = maxY;
   }
 
   move(key) {    
     if (key === 'ArrowRight') {
-      this.xpos += 85;
-      this.element.style.backgroundPositionY = '0';
+      if (this.xpos < this.maxX-1){
+        this.xpos += 1;
+        this.element.style.backgroundPositionY = '0';
+      }
     }
     else if (key === 'ArrowLeft') {
-      this.xpos -= 85;
-      this.element.style.backgroundPositionY = '255px';
+      if (this.xpos > 0) {
+        this.xpos -= 1;
+        this.element.style.backgroundPositionY = '255px';
+      }
     }
     else if (key === 'ArrowUp') {
-      this.ypos -= 85;
-      this.element.style.backgroundPositionY = '85px';
+      if (this.ypos > 0) {
+        this.ypos -= 1;
+        this.element.style.backgroundPositionY = '85px';
+      }
     }
     else if (key === 'ArrowDown') {
-      this.ypos += 85;
-      this.element.style.backgroundPositionY = '170px';
+      if (this.ypos < this.maxY-1){
+        this.ypos += 1;
+        this.element.style.backgroundPositionY = '170px';
+      }
     }
     this.update();
   }
@@ -55,19 +65,43 @@ class Pacman {
 
   update() {
     // this.element = document.querySelector('.entity--pac');
-    this.element.style.left = `${this.xpos}px`;
-    this.element.style.top = `${this.ypos}px`;
+    this.element.style.left = `${this.xpos * 85}px`;
+    this.element.style.top = `${this.ypos * 85}px`;
   }
+
+  mount() {
+    const stageElem = document.querySelector('.stage');
+    this.render();
+    stageElem.appendChild(this.element);
+  }
+}
+
+class Stage {
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  render() {
+    this.element = document.createElement('div');
+    this.element.className = 'stage';
+    this.element.style.width = `${this.width * 85}px`;
+    this.element.style.height = `${this.height * 85}px`;  }
+
+  // update() {
+
+  // }
 
   mount() {
     const contElem = document.querySelector('.container');
     this.render();
     contElem.appendChild(this.element);
   }
-  
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const pacman = new Pacman(0, 0, 0);
+  const stage = new Stage(12, 6);
+  stage.mount();
+  const pacman = new Pacman(0, 0, 0, stage.width, stage.height);
   pacman.mount();
 });
