e11c77a37b3cf9f9f5fb10cf4c537c20f4753214
ondrejtrisc <57498031+ondrejtrisc@users.noreply.github.com>
2020-02-07 09:57:29 +0100

    pacman closes its mouth

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('it works');
+  const pacElement = document.querySelector('.entity--pac');
+  document.addEventListener('keydown', event => {
+    if(event.key === 'ArrowRight') {
+      if (pacElement.style.backgroundPositionX === '85px') {
+        pacElement.style.backgroundPositionX = '0';
+      }
+      else {
+        pacElement.style.backgroundPositionX = '85px';
+      }
+    }
+  });
 });
